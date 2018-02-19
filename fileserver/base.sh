333bdd38f5afabcac52c6262b8c5961823927db3
Pierre Penninckx <ibizapeanut@gmail.com>
2018-02-19 09:56:13 -0800

    add default fcron file

--- fileserver/base.sh
+++ fileserver/base.sh
@@ -19,6 +19,19 @@ function run() {
         vim \
         || exit 1
 
+    cat <<FCRONTAB | fcrontab -
+    # * * * * *
+    # | | | | |
+    # | | | | +---- Day of the Week   (range: 1-7, 1 standing for Monday)
+    # | | | +------ Month of the Year (range: 1-12)
+    # | | +-------- Day of the Month  (range: 1-31)
+    # | +---------- Hour              (range: 0-23)
+    # +------------ Minute            (range: 0-59)
+
+    !erroronlymail(true)
+FCRONTAB
+
     systemctl start fcron
     systemctl enable fcron
+
 }
