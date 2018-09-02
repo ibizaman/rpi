a1e4261c1f5014a8b9ff43239ae67c688817a6df
ibizaman <ibizapeanut@gmail.com>
2018-09-02 00:07:52 -0700

    fix indentation in default fcrontab

--- fileserver/base.sh
+++ fileserver/base.sh
@@ -22,16 +22,16 @@ function install_remote() {
         vim \
         || exit 1
 
-    cat | fcrontab - <<FCRONTAB
-    # * * * * *
-    # | | | | |
-    # | | | | +---- Day of the Week   (range: 1-7, 1 standing for Monday)
-    # | | | +------ Month of the Year (range: 1-12)
-    # | | +-------- Day of the Month  (range: 1-31)
-    # | +---------- Hour              (range: 0-23)
-    # +------------ Minute            (range: 0-59)
+    cat | fcrontab - <<-FCRONTAB
+# * * * * *
+# | | | | |
+# | | | | +---- Day of the Week   (range: 1-7, 1 standing for Monday)
+# | | | +------ Month of the Year (range: 1-12)
+# | | +-------- Day of the Month  (range: 1-31)
+# | +---------- Hour              (range: 0-23)
+# +------------ Minute            (range: 0-59)
 
-    !erroronlymail(true)
+!erroronlymail(true)
 FCRONTAB
 
     systemctl daemon-reload
