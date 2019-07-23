1b55747ca91ff66bfd0954d949012e8662d0c6e8
ibizaman <ibizapeanut@gmail.com>
2019-07-22 22:46:14 -0700

    make aria2 use vpn

--- fileserver/aria2.sh
+++ fileserver/aria2.sh
@@ -50,8 +50,11 @@ function install_remote() {
 
     cd /opt/ || exit 1
 
-    git clone https://github.com/ziahamza/webui-aria2.git || (cd webui-aria2 && git pull)
+    if ! grep aria2 /etc/iproute2/rt_tables; then
+        echo -e "\n10 aria2" >> /etc/iproute2/rt_tables
+    fi
 
+    git clone https://github.com/ziahamza/webui-aria2.git || (cd webui-aria2 && git pull)
     sed -i "s|// token: '\$YOUR_SECRET_TOKEN\$'|  token: '$aria2_secret'|" /opt/webui-aria2/src/js/services/configuration.js
 
     groupadd --system downloader
@@ -73,19 +76,21 @@ ARIA2CONF
     cat > /etc/systemd/system/aria2.service <<ARIA2SERVICE
 [Unit]
 Description=Aria2 Service
-After=network.target
+After=openvpn-client@privateinternetaccess.service
 
 [Service]
 User=aria2
 Group=aria2
 ExecStart=/usr/bin/aria2c \\
               --enable-rpc \\
-              --rpc-listen-all \\
               --rpc-allow-origin-all \\
+              --async-dns=false  \\
               --interface=tun0 \\
+              --bt-lpd-interface wlan0 \\
               --save-session /var/lib/aria2/session.lock \\
               --input-file /var/lib/aria2/session.lock \\
-              --conf-path=/etc/aria2/aria2.conf
+              --conf-path=/etc/aria2/aria2.conf \\
+              --continue
 
 [Install]
 WantedBy=default.target
@@ -107,6 +112,72 @@ WantedBy=default.target
 ARIA2WEBSERVICE
 
     chown -R aria2: /opt/webui-aria2
+
+    cat << ARIA2ROUTEUP > /etc/openvpn/client/privateinternetaccess/conf_up/aria2_up.sh
+#! /bin/bash
+
+set -x
+
+# add the vpn device as default route for this routing table
+ip route add default via \$route_vpn_gateway dev \$dev table aria2
+
+# add rules that all traffic going to the gateway as well as
+# all traffic comming from my local VPN is routed through the
+# VPN's gateway
+ip rule add from \$ifconfig_local/32 table aria2
+ip rule add to \$route_vpn_gateway/32 table aria2
+
+# and flush the cache to make sure that the changes were commited
+ip route flush cache
+
+iptables -A OUTPUT -o wlan0 -m owner --uid-owner \$(id -u aria2) -j DROP
+iptables -A OUTPUT -o eth0 -m owner --uid-owner \$(id -u aria2) -j DROP
+exit 0
+ARIA2ROUTEUP
+    chmod a+x /etc/openvpn/client/privateinternetaccess/conf_up/aria2_up.sh
+
+    cat << ARIA2ROUTEDOWN > /etc/openvpn/client/privateinternetaccess/conf_down/aria2_down.sh
+#! /bin/bash
+
+set -x
+
+# add the vpn device as default route for this routing table
+ip route del default via \$route_vpn_gateway dev \$dev table aria2
+
+# add rules that all traffic going to the gateway as well as
+# all traffic comming from my local VPN is routed through the
+# VPN's gateway
+ip rule del from \$ifconfig_local/32 table aria2
+ip rule del to \$route_vpn_gateway/32 table aria2
+
+# and flush the cache to make sure that the changes were commited
+ip route flush cache
+
+iptables -D OUTPUT -o wlan0 -m owner --uid-owner \$(id -u aria2) -j DROP
+iptables -D OUTPUT -o eth0 -m owner --uid-owner \$(id -u aria2) -j DROP
+exit 0
+ARIA2ROUTEDOWN
+    chmod a+x /etc/openvpn/client/privateinternetaccess/conf_down/aria2_down.sh
+
+    systemctl restart openvpn-client@privateinternetaccess
+
+#    cat > /etc/systemd/system/elm-torrent.service <<ELMTORRENT
+#[Unit]
+#Description=Elm Torrent
+#After=network.target
+#
+#[Service]
+#User=elm-torrent
+#Group=elm-torrent
+#WorkingDirectory=/opt/elm-torrent
+#ExecStart=/usr/bin/node /opt/elm-torrent/node-server.js
+#
+#[Install]
+#WantedBy=default.target
+#ELMTORRENT
+
+#    chown -R elm-torrent: /opt/elm-torrent
+#    useradd --home-dir /opt/elm-torrent elm-torrent
 
     pip install --upgrade jsondispatch
     useradd --system jsondispatch
