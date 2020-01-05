fb43153f3f95e474fe730d11d61511899d5d094c
ibizaman <ibizapeanut@gmail.com>
2020-01-05 15:48:32 -0800

    update ums to 9.1.0 and fix some install issues

--- fileserver/universalmediaserver.sh
+++ fileserver/universalmediaserver.sh
@@ -6,6 +6,8 @@ function arguments() {
 
 
 function install_remote() {
+    UMS_VERSION=9.1.0
+
     pacman -Syu --noconfirm --needed \
         fontconfig \
         mediainfo \
@@ -15,28 +17,30 @@ function install_remote() {
         || exit 1
 
 
-    cd /opt/ || exit 1
+    pushd /opt/ || exit 1
 
     #[ -d tsmuxer-2.6.11 ] || curl -L -O https://www.deb-multimedia.org/pool/non-free/t/tsmuxer/tsmuxer_2.6.11.orig.tar.gz || exit 1
     #tar xf tsmuxer_2.6.11.orig.tar.gz
 
-    [ -d ums-6.8.0 ] || curl -L -O http://downloads.sourceforge.net/project/unimediaserver/Official%20Releases/Linux/UMS-6.8.0.tgz || exit 1
-    tar xf UMS-6.8.0.tgz
+    [ -d ums-${UMS_VERSION} ] || curl -L -O http://downloads.sourceforge.net/project/unimediaserver/Official%20Releases/Linux/UMS-${UMS_VERSION}.tgz || exit 1
+    tar xf UMS-${UMS_VERSION}.tgz || exit 1
 
-    rm /opt/ums-6.8.0/linux/ffmpeg
-    rm /opt/ums-6.8.0/linux/ffmpeg64
+    #rm /opt/ums-6.8.0/linux/ffmpeg
+    #rm /opt/ums-6.8.0/linux/ffmpeg64
     #rm /opt/ums-6.8.0/linux/tsMuxeR
     #rm /opt/ums-6.8.0/linux/tsMuxeR-new
-    ln -s /usr/bin/ffmpeg /opt/ums-6.8.0/linux/ffmpeg
-    ln -s /usr/bin/ffmpeg /opt/ums-6.8.0/linux/ffmpeg64
+    #ln -s /usr/bin/ffmpeg /opt/ums-6.8.0/linux/ffmpeg
+    #ln -s /usr/bin/ffmpeg /opt/ums-6.8.0/linux/ffmpeg64
     #ln -s /opt/tsmuxer-2.6.11/tsMuxeR /opt/ums-6.8.0/linux/tsMuxeR
     #ln -s /opt/tsmuxer-2.6.11/tsMuxeR /opt/ums-6.8.0/linux/tsMuxeR-new
 
-    cp /opt/ums-6.8.0/
+    pushd /opt/ums-${UMS_VERSION}/ || exit 1
 
-    useradd --home-dir /opt/ums-6.8.0 --system ums
+    systemctl stop ums
 
-    chown -R ums: /opt/ums-6.8.0
+    useradd --home-dir /opt/ums-${UMS_VERSION} --system ums \
+	|| usermod --home /opt/ums-${UMS_VERSION} ums \
+	|| exit 1
 
     # From https://aur.archlinux.org/cgit/aur.git/tree/ums.service?h=ums
     cat > /etc/systemd/system/ums.service <<UMSSERVICE
@@ -49,19 +53,25 @@ After=syslog.target network-online.target rpcbind.service
 #Environment="UMS_MAX_MEMORY=1280M"
 User=ums
 Group=ums
-WorkingDirectory=/opt/ums-6.8.0/
+WorkingDirectory=/opt/ums-${UMS_VERSION}/
 Type=simple
-ExecStart=/opt/ums-6.8.0/UMS.sh
+ExecStart=/opt/ums-${UMS_VERSION}/UMS.sh
 
 [Install]
 WantedBy=multi-user.target
 UMSSERVICE
 
+    systemctl daemon-reload
     systemctl enable ums
 
-    cp /opt/ums-6.8.0/UMS.conf /opt/ums-6.8.0/.config/UMS/UMS.conf
+    if [ ! -f /opt/ums-${UMS_VERSION}/.config/UMS/UMS.conf ]; then
+	mkdir -p /opt/ums-${UMS_VERSION}/.config/UMS
+        cp /opt/ums-${UMS_VERSION}/UMS.conf /opt/ums-${UMS_VERSION}/.config/UMS/UMS.conf
+    fi
 
-    echo add individual folders in '/srv/' to folders and folders_monitored in /opt/ums-6.8.0/.config/UMS/UMS.conf
+    chown -R ums: /opt/ums-${UMS_VERSION}
+
+    echo add individual folders in '/srv/' to folders and folders_monitored in /opt/ums-${UMS_VERSION}/.config/UMS/UMS.conf
 }
 
 function install_local() {
